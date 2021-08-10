8b395f6bc281069ae3994467a1c96c9e5744d455
Jose D. Vasquez <jodavaga@gmail.com>
2021-08-09 21:09:28 -0500

    remove project files

--- src/contexts/ContextProvider.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import { listaLibros } from "../assets/data/libros";
-
-export const AppContext = React.createContext();
-
-export default function AppProvider({ children, ...rest }) {
-  return <AppContext.Provider {...rest}>{children}</AppContext.Provider>;
-}
